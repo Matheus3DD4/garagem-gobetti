8e22dc92f6f8a88fab28d877c55e8ddf057e51a0
Matheus3DD4 <matheus3dd4@gmail.com>
2023-05-16 15:34:12 -0300

    first commit

---
+++ config/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import include, path
+
+from rest_framework.routers import DefaultRouter
+
+from garagem.views import AcessorioViewSet, CategoriaViewSet, CorViewSet, MarcaViewSet, VeiculoViewSet
+
+router = DefaultRouter()
+router.register(r"acessorios", AcessorioViewSet)
+router.register(r"categorias", CategoriaViewSet)
+router.register(r"cores", CorViewSet)
+router.register(r"marcas", MarcaViewSet)
+router.register(r"veiculos", VeiculoViewSet)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include(router.urls)),
+]
