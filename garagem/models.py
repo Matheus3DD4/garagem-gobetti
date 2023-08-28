40353a12eaded8545655823c51e566212696316e
Matheus3DD4 <matheus3dd4@gmail.com>
2023-08-28 12:22:36 -0300

    many to many

--- garagem/models.py
+++ garagem/models.py
@@ -10,7 +10,8 @@ class Marca(models.Model):
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
 
-def __str__(self):return  self.descricao
+    def __str__(self):
+        return  self.descricao
     
 class Acessorio(models.Model):
     descricao = models.CharField(max_length=100)
@@ -36,6 +37,7 @@ class Veiculo(models.Model):
     marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos")
     cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="veiculos")
+    acessorio = models.ManyToManyField(Acessorio, related_name="veiculo")
 
     def __str__(self):
         return f"{self.marca} {self.categoria} {self.cor} {self.ano}"
